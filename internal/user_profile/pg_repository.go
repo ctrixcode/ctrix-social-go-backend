cfa5d1f4a5040d50a93b19d2c5a2496b30938298
Chandrashekhar Kachawa <42296139+ctrixcode@users.noreply.github.com>
2025-09-12 21:54:01 +0530

    feat: added user profile

---
+++ internal/user_profile/pg_repository.go
@@ -0,0 +1,87 @@
+package user_profile
+
+import (
+	"database/sql"
+
+	sq "github.com/Masterminds/squirrel"
+	"github.com/jmoiron/sqlx"
+)
+
+type pgProfileRepository struct {
+	db *sqlx.DB
+	sq sq.StatementBuilderType
+}
+
+func NewRepository(db *sqlx.DB) ProfileRepository {
+	return &pgProfileRepository{
+		db: db,
+		sq: sq.StatementBuilder.PlaceholderFormat(sq.Dollar),
+	}
+}
+
+func (r *pgProfileRepository) CreateProfile(profile *UserProfile) error {
+	query, args, err := r.sq.Insert("users_profile").
+		Columns("id", "first_name", "last_name", "profile_picture", "avatar", "relation_status", "dob", "bio", "gender", "family_members", "hobbies").
+		Values(profile.ID, profile.FirstName, profile.LastName, profile.ProfilePicture, profile.Avatar, profile.RelationStatus, profile.Dob, profile.Bio, profile.Gender, profile.FamilyMembers, profile.Hobbies).
+		ToSql()
+	if err != nil {
+		return err
+	}
+
+	_, err = r.db.Exec(query, args...)
+	return err
+}
+
+func (r *pgProfileRepository) GetProfileByID(id string) (*UserProfile, error) {
+	var profile UserProfile
+	query, args, err := r.sq.Select("*").
+		From("users_profile").
+		Where(sq.Eq{"id": id}).
+		ToSql()
+	if err != nil {
+		return nil, err
+	}
+
+	err = r.db.Get(&profile, query, args...)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return nil, nil
+		}
+		return nil, err
+	}
+	return &profile, nil
+}
+
+func (r *pgProfileRepository) UpdateProfile(profile *UserProfile) error {
+	query, args, err := r.sq.Update("users_profile").
+		Set("first_name", profile.FirstName).
+		Set("last_name", profile.LastName).
+		Set("profile_picture", profile.ProfilePicture).
+		Set("avatar", profile.Avatar).
+		Set("relation_status", profile.RelationStatus).
+		Set("dob", profile.Dob).
+		Set("bio", profile.Bio).
+		Set("gender", profile.Gender).
+		Set("family_members", profile.FamilyMembers).
+		Set("hobbies", profile.Hobbies).
+		Where(sq.Eq{"id": profile.ID}).
+		ToSql()
+	if err != nil {
+		return err
+	}
+
+	_, err = r.db.Exec(query, args...)
+	return err
+}
+
+func (r *pgProfileRepository) DeleteProfile(id string) error {
+	query, args, err := r.sq.Delete("users_profile").
+		Where(sq.Eq{"id": id}).
+		ToSql()
+	if err != nil {
+		return err
+	}
+
+	_, err = r.db.Exec(query, args...)
+	return err
+}
---
+++ internal/user_profile/repository.go
@@ -0,0 +1,8 @@
+package user_profile
+
+type ProfileRepository interface {
+	CreateProfile(profile *UserProfile) error
+	GetProfileByID(id string) (*UserProfile, error)
+	UpdateProfile(profile *UserProfile) error
+	DeleteProfile(id string) error
+}

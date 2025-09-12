fcd4bb57eb9796b4a7d776cca2533183c07dfade
Chandrashekhar Kachawa <42296139+ctrixcode@users.noreply.github.com>
2025-09-12 21:54:47 +0530

    fix: pointer bug

--- internal/user_profile/model.go
+++ internal/user_profile/model.go
@@ -5,17 +5,17 @@ import "time"
 // UserProfile represents a user's profile information stored in the database.
 // The ID field is a foreign key referencing the users_auth table.
 type UserProfile struct {
-	ID             string    // Unique identifier, foreign key to users_auth.id
-	FirstName      string    // User's first name
-	LastName       string    // User's last name
-	ProfilePicture string    // URL or path to the user's profile picture
-	Avatar         string    // Identifier for a user's avatar
-	RelationStatus string    // User's relationship status
-	Dob            time.Time // Date of birth
-	Bio            string    // User's biography
-	Gender         string    // User's gender
-	FamilyMembers  []string  // Array of family member IDs or names
-	Hobbies        []string  // Array of user's hobbies
-	CreatedAt      time.Time // Timestamp of profile creation
-	UpdatedAt      time.Time // Timestamp of last profile update
+	ID             string     // Unique identifier, foreign key to users_auth.id
+	FirstName      string     // User's first name
+	LastName       string     // User's last name
+	ProfilePicture string     // URL or path to the user's profile picture
+	Avatar         string     // Identifier for a user's avatar
+	RelationStatus string     // User's relationship status
+	Dob            *time.Time // Date of birth
+	Bio            string     // User's biography
+	Gender         string     // User's gender
+	FamilyMembers  []string   // Array of family member IDs or names
+	Hobbies        []string   // Array of user's hobbies
+	CreatedAt      *time.Time // Timestamp of profile creation
+	UpdatedAt      *time.Time // Timestamp of last profile update
 }

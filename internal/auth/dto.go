13eea20670f5441fb7dc9183d3778748a8a3f466
Chandrashekhar Kachawa <42296139+ctrixcode@users.noreply.github.com>
2025-09-13 10:16:29 +0530

    feat: dto for user auth

---
+++ internal/auth/dto.go
@@ -0,0 +1,21 @@
+package auth
+
+type RegisterRequest struct {
+	Email    string `json:"email" validate:"required,email"`
+	Username string `json:"username" validate:"required,min=3,max=20"`
+	Password string `json:"password" validate:"required,min=8"`
+}
+
+type LoginRequest struct {
+	Email    string `json:"email" validate:"required,email"`
+	Password string `json:"password" validate:"required"`
+}
+
+type RefreshTokenRequest struct {
+	RefreshToken string `json:"refreshToken" validate:"required"`
+}
+
+type AuthResponse struct {
+	AccessToken  string `json:"accessToken"`
+	RefreshToken string `json:"refreshToken"`
+}

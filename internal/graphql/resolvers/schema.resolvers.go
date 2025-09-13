a140f2940088b39eb86433872a60c57709ab7f26
Chandrashekhar Kachawa <42296139+ctrixcode@users.noreply.github.com>
2025-09-13 14:50:57 +0530

    feat: graphql working now

--- internal/graphql/resolvers/schema.resolvers.go
+++ internal/graphql/resolvers/schema.resolvers.go
@@ -18,7 +18,7 @@ func (r *mutationResolver) SayHello(ctx context.Context, name string) (string, e
 
 // Hello is the resolver for the hello field.
 func (r *queryResolver) Hello(ctx context.Context) (string, error) {
-	panic(fmt.Errorf("not implemented: Hello - hello"))
+	return "Hello world!", nil
 }
 
 // Mutation returns generated.MutationResolver implementation.
